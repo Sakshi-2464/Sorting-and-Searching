d77ca51bdc2da7893754324ebbdd0b453cb23224
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-06-30 22:16:22 +0530

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -4,17 +4,21 @@ int main() {
     printf("enter the size of array: "); //ask for the size of the array
     scanf("%d",&n); //take input n as array size
     printf("enter the elements: "); //ask the user to enter elements
+    
     // for loop to take user input for each element in array
     for(i=0;i<n;i++){ 
         scanf("%d",&a[i]);
     }
-    // we enter element which is sorted by default
+
+    // insertion sort
+    // first element is sorted by default
     // hence we start with second element -> j=1
     for(j=1;j<n;j++){
         key=a[j]; 
         // store the value at j in variable key - this is the value to be compared
         i=j-1; 
         // i points to the element which is the to the left of the element pointed by j
+        
         // check the value of i and the value at i if it is greater than key
         while(i>=0 && a[i]>key){ 
             a[i+1]=a[i]; // if condtion is true move the element to right
@@ -23,10 +27,13 @@ int main() {
         a[i+1]=key; // put key at correct position
     }
     
+    // displaying sorted array
     printf("sorted elements:");
+    
     // for loop to show each element of the output array
     for(i=0;i<n;i++){
         printf(" %d",a[i]);
     }
+    
     return 0;
 }
