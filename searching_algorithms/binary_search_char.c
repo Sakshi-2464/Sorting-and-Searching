a5b5370e3b907e95789f3f4fdb9824f4c43d6cd0
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-07-01 11:52:08 +0530

    Create binary_search_char.c

---
+++ searching_algorithms/binary_search_char.c
@@ -0,0 +1,50 @@
+#include <stdio.h> //standard input output library
+
+// binay search function
+int binary_search(char a[], int n, char x) {
+    int l = 0; // lower bound
+    int r = n - 1; //upper bound
+
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        if (a[mid] == x) {
+            return mid; // Element found
+        } else if (a[mid] < x) {
+            l = mid + 1; // search in right side array
+        } else {
+            r = mid - 1; // search in left side array
+        }
+    }
+
+    return -1; // Element not found
+}
+
+int main() {
+    int n;
+    char x; // declare variables
+// take user input
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    char a[n];
+
+    printf("Enter the elements of the array in sorted order:\n");
+    for (int i = 0; i < n; i++) {
+        scanf(" %c", &a[i]);
+    }
+
+    printf("Enter the element to search for: ");
+    scanf(" %c", &x);
+
+    int index = binary_search(a, n, x); // function call store the return value in index
+    
+// check for index
+    if (index != -1) {
+        printf("Element %c found at index %d.\n", x, index);
+    } else {
+        printf("Element %c not found in the array.\n", x);
+    }
+
+    return 0;
+}
