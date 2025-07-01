65c4aef86692557eda447a6c8f895f2a2b66f2f6
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-07-01 17:42:10 +0530

    Update quick_sort_descending_char.c

--- sorting_algorithms/quick_sort_descending_char.c
+++ sorting_algorithms/quick_sort_descending_char.c
@@ -10,26 +10,27 @@ int partition(char a[], int l, int r){
     char pivot=a[r];
     int i=l-1;
     // Initialize i to one position before the start of the subarray
-// i will track the last element smaller than the pivot.
+// i will track the last element greater than the pivot.
     for(int j=l;j<r;j++){
            // j scans the array from the left (l) to one before the pivot (r - 1)
         if(a[j]>pivot){
-             // If current element is smaller than the pivot...
+             // If current element is greater than the pivot...
             i++;
-             // ...we move the boundary of smaller elements by increasing i
+             // ...we move the boundary of greater elements by increasing i
             swap(&a[i],&a[j]);
             // ...and swap the current element with the element at i.
-        // This places the smaller element in the correct partition.
+        // This places the greater element in the correct partition.
         }
     }
     swap(&a[i+1],&a[r]);
     // After the loop, place the pivot in its correct position:
-// i + 1 is the first index where pivot is greater than all to its left.
+// i + 1 is the first index where pivot is smaller than all to its left.
 // So, we swap pivot (a[r]) with a[i+1].
     return i+1;
     // Return the index of the pivot's correct position.
 // This is used to recursively apply quick sort to left and right subarrays.
 }
+// now all th elemnets to the left of pivot are greater then pivot and that to the right are smaller than pivot
 // quick sort function, recurssive call
 void quick_sort(char a[], int l, int r){
     if(l<r){
