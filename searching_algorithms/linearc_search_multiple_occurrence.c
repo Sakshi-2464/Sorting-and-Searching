17d3de299f05903c0d8e820e98ee813311dcd210
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-07-01 11:37:34 +0530

    Update linearc_search_multiple_occurrence.c

--- searching_algorithms/linearc_search_multiple_occurrence.c
+++ searching_algorithms/linearc_search_multiple_occurrence.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 // linear search function
-int linear_search(int a[],int n, int x){
+void linear_search(int a[],int n, int x){
+    int found=0;
     for(int i=0;i<n;i++){
-        // search for the value and return the corresponding index
         if(a[i]==x){
-            return i;
+            printf("element found at index %d\n",i);
+            found=1;
         }
     }
-    return -1;
+    if(!found){
+        printf("element not found");
+    }
 }
 int main() {
     int n,x; // declare variables
@@ -23,13 +26,7 @@ int main() {
     // input element to search
 printf("eneter element to search: ");
 scanf("%d",&x);
-int index= linear_search(a,n,x); //function call
-// check and return the index
-if(index!=-1){
-    printf("element found at index %d",index);
-}
-else{
-    printf("element not found");
-}
+// function call
+linear_search(a,n,x);
     return 0;
 }
