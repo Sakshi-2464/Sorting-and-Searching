a76d98120f63d0ee74a873e37bb6f229f3845f43
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-07-01 12:02:03 +0530

    Create insertion_sort_descending_char.c

---
+++ sorting_algorithms/insertion_sort_descending_char.c
@@ -0,0 +1,28 @@
+#include <stdio.h> //standard input output library
+int main() {
+    int n,i,j;
+    char key;// declare variables
+// take user input
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    char a[n];
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < n; i++) {
+        scanf(" %c", &a[i]);
+    }
+    // insertion sort
+    for(j=1;j<n;j++){
+        key=a[j];
+        i=j-1;
+        while(i>=0 && a[i]<key){
+            a[i+1]=a[i];
+            i--;
+        }
+        a[i+1]=key;
+    }
+    printf("sorted array:");
+    for (i = 0; i < n; i++) {
+        printf(" %c", a[i]);
+    }
+    return 0;
+}
