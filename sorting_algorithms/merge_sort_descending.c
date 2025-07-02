735d7dd61789df6ea6d4226762709dc2f779a991
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-07-02 19:39:22 +0530

    Create merge_sort_descending.c

---
+++ sorting_algorithms/merge_sort_descending.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+// merge function
+void merge(int a[], int l, int mid, int r){
+    int i,j,k; // declare variables
+    // find the size of 2 sub arrays
+    int n1=mid-l+1;
+    int n2=r-mid;
+    // declare the temporary sub arrays
+    int L[n1];
+    int R[n2];
+    // copy contents to these arrays
+    for(i=0;i<n1;i++){
+        L[i]=a[l+i];
+    }
+    for(j=0;j<n2;j++){
+        R[j]=a[mid+1+j];
+    }
+    
+    i=0; // index foe L[]
+    j=0; // index for R[]
+    k=l; // index for merged sub array a[]
+    
+    // append the elements according to the values in a[]
+    while(i<n1 && j<n2){
+        if(L[i]>=R[j]){
+            a[k++]=L[i++]; // place L[i] into a[k], then increment i and k
+        }
+        else{
+            a[k++]=R[j++]; // place R[j] into a[k], then increment j and k
+        }
+    }
+    // append remaining elements of L[] if any
+    while(i<n1){
+        a[k++]=L[i++];
+    }
+    // append remaining elements of R[] if any
+    while(j<n2){
+        a[k++]=R[j++];
+    }
+}
+// merge sort
+void merge_sort(int a[],int l, int r){
+    if(l<r){
+        int mid=(l+r) /2;
+        // recurssive function calls
+        // sort the two halves
+        merge_sort(a,l,mid);
+        merge_sort(a,mid+1,r);
+        // after sorting merge the sorted halves
+        merge(a,l,mid,r);
+    }
+}
+int main(){
+    int n,i,j,k;
+    printf("enter size:");
+    scanf("%d",&n);
+    int a[n];
+    printf("enter elements:");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    merge_sort(a,0,n-1);
+    printf("sorted elements:");
+    for(i=0;i<n;i++){
+        printf(" %d",a[i]);
+    }
+    return 0;
+}
