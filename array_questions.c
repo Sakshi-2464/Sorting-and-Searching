d4d4f0d23ed0f615010c03cc7003a4a157286fd1
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-07-05 18:14:12 +0530

    Update array_questions.c

--- array_questions.c
+++ array_questions.c
@@ -242,4 +242,18 @@ int main() {
     }
     return 0;
 }
+// swapping adjacent elements in array
+#include<stdio.h>
+int main(){
+    int a[]={2,50,56,4,8};
+    int n=sizeof(a)/sizeof(a[0]);
+    for(int i=0;i<n-1;i+=2){
+        int temp=a[i];
+        a[i]=a[i+1];
+        a[i+1]=temp;
+    }
+    for(int i=0;i<n;i++){
+        printf(" %d",a[i]);
+    }
+}
 //
