f9953bd2094a125fbebab6f21ca00a1148f96f0c
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-07-06 21:19:32 +0530

    Update array_questions.c

--- array_questions.c
+++ array_questions.c
@@ -95,6 +95,21 @@ int main(){
     }
     return 0;
 }
+// right rotate array by 1 place
+#include<stdio.h>
+int main(){
+    int a[]={2,5,0,4,8,3};
+    int n=sizeof(a)/sizeof(a[0]);
+    int temp=a[n-1];
+    for(int i=n-1;i>0;i--){
+        a[i]=a[i-1];
+    }
+    a[0]=temp;
+    for(int i=0;i<n;i++){
+        printf(" %d",a[i]);
+    }
+    return 0;
+}
 // second largest element in array
 #include<stdio.h>
 int main(){
