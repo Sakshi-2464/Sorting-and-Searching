9520e94a23f4b5e7c32819f593616754d74a7384
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-07-27 01:00:05 +0530

    Update array_questions.c

--- array_questions.c
+++ array_questions.c
@@ -839,6 +839,105 @@ int main() {
     }
     return 0;
 }
+// to get maximum row sum and colum sum in 2d array along with the row number and col number
+#include <stdio.h>
+int main() {
+    int rows,cols,i,j;
+    printf("enter number of rows:");
+    scanf("%d",&rows);
+    printf("enter number of columns:");
+    scanf("%d",&cols);
+    int a[rows][cols];
+    printf("enter the values row-wise:");
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+                printf("%d\t",a[i][j]);
+            }
+            printf("\n");
+    }
+    int row_sum = 0, col_sum = 0;
+    int max_row_sum = 0, max_col_sum = 0;
+    int max_row_index = 0, max_col_index = 0;
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            row_sum=row_sum+a[i][j];
+        }
+        printf("sum of the elements in row %d = %d\n",i+1,row_sum);
+        if(row_sum>max_row_sum){max_row_sum=row_sum;
+            max_row_index=i+1;
+        }
+        row_sum=0;
+    }
+    printf("row %d has maximum sum as %d\n",max_row_index,max_row_sum);
+    for(j=0;j<cols;j++){
+        for(i=0;i<rows;i++){
+            col_sum=col_sum+a[i][j];
+        }
+        printf("sum of the elements in column %d = %d\n",j+1,col_sum);
+        if(col_sum>max_col_sum){
+            max_col_sum=col_sum;
+            max_col_index=j+1;
+        }
+        col_sum=0;
+    }
+    printf("column %d has maximum sum as %d\n",max_col_index,max_col_sum);
+    return 0;
+}
+// to get minimum row sum and col sum along with row number and col number
+#include <stdio.h>
+#include<limits.h>
+int main() {
+    int rows,cols,i,j;
+    printf("enter number of rows:");
+    scanf("%d",&rows);
+    printf("enter number of columns:");
+    scanf("%d",&cols);
+    int a[rows][cols];
+    printf("enter the values row-wise:");
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+                printf("%d\t",a[i][j]);
+            }
+            printf("\n");
+    }
+    int row_sum = 0, col_sum = 0;
+    int min_row_sum = INT_MAX, min_col_sum = INT_MAX;
+    int min_row_index = 0, min_col_index = 0;
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            row_sum=row_sum+a[i][j];
+        }
+        printf("sum of the elements in row %d = %d\n",i+1,row_sum);
+        if(row_sum<min_row_sum){min_row_sum=row_sum;
+            min_row_index=i+1;
+        }
+        row_sum=0;
+    }
+    printf("row %d has maximum sum as %d\n",min_row_index,min_row_sum);
+    for(j=0;j<cols;j++){
+        for(i=0;i<rows;i++){
+            col_sum=col_sum+a[i][j];
+        }
+        printf("sum of the elements in column %d = %d\n",j+1,col_sum);
+        if(col_sum<min_col_sum){
+            min_col_sum=col_sum;
+            min_col_index=j+1;
+        }
+        col_sum=0;
+    }
+    printf("column %d has maximum sum as %d\n",min_col_index,min_col_sum);
+    return 0;
+}
 // transpose of matrix
 #include <stdio.h>
 int main() {
