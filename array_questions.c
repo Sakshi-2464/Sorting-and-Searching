dafc5b679dde6a05453eae73311cb4e2f2ecf0d2
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-07-27 00:17:43 +0530

    Update array_questions.c

--- array_questions.c
+++ array_questions.c
@@ -762,4 +762,43 @@ int main() {
     }
     return 0;
 }
+// searching an element in 2d array
+#include <stdio.h>
+int main() {
+    int rows,cols,i,j;
+    printf("enter number of rows:");
+    scanf("%d",&rows);
+    printf("enter number of columns:");
+    scanf("%d",&cols);
+    int a[rows][cols];
+    printf("enter the values row-wise:");
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+                printf("%d\t",a[i][j]);
+            }
+            printf("\n");
+    }
+    printf("enter the target element:");
+    int target;
+    int found=0;
+    scanf("%d",&target);
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            if(a[i][j]==target){
+                printf("found at (%d,%d)\n",i+1,j+1); // Adding 1 to index to display position in 1-based indexing
+
+                found=1;
+            }
+        }
+    }
+    if(!found){
+        printf("element not found");
+    }
+    return 0;
+}
 //
