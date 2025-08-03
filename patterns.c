b68dd2134aa60aecc637f6b4f004da279709eaff
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-08-03 20:21:15 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -3,8 +3,52 @@ int main(){
     int n;
     printf("enter value for n:");
     scanf("%d",&n);
-    for(int i=0;i<=n;i++){
-        for(int j=i+1;j<=n;j++){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+           printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+/*
+enter value for n:5
+*
+**
+***
+****
+*****
+    */
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter value for n:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+           printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+/*
+enter value for n:5
+*****
+*****
+*****
+*****
+*****
+    */
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter value for n:");
+    scanf("%d",&n);
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
            printf("*");
         }
         printf("\n");
@@ -19,3 +63,4 @@ enter value for n:5
 **
 *
 */
+
