ab1961b52863176a1053274306d0900be564c6fc
Sakshi Tupkar <122149939+Sakshi-2464@users.noreply.github.com>
2025-08-03 20:22:57 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -64,3 +64,25 @@ enter value for n:5
 *
 */
 
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter value for n:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",j);
+    }
+        printf("\n");
+    }
+    return 0;
+}
+/*
+enter value for n:5
+1
+12
+123
+1234
+12345
+*/
+
